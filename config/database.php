b01a536a0694aced49b1f0bb164d73dc9a0a226e
Abdulrahman Ahmed <ikarzma37@gmail.com>
2025-11-11 17:45:21 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -7,7 +7,6 @@ $password = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->exec("SET NAMES utf8mb4");
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
